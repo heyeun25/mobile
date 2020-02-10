4a5fcf8e6cdab40cbe3356b68743754b0cb83b00
hyeeun park <h514.park@samsung.com>
2020-02-10 13:13:06 +0900

    fractal tree
    Former-commit-id: 0ee3d0f6b1be3f5f18db1d54a2a3766f8adcd92f

---
+++ new-shelf/src/components/Tree.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <canvas class="treeCanvas" ref="treeCanvas">
+
+    </canvas>
+  </div>
+</template>
+
+<script>
+
+// var canvasEl;
+// var ctx;
+
+export default {
+  name: 'Tree',
+  props: {
+    msg: String
+  },
+  methods: {
+    draw: function(startX, startY, len, angle, branchWidth) {
+      let ctx = this.$refs.treeCanvas.getContext("2d");
+      ctx.beginPath();
+      ctx.save();
+      
+      ctx.translate(startX, startY);
+      ctx.rotate(angle * Math.PI/180);
+      ctx.moveTo(0, 0);
+      // if(angle > 0) {
+      //     ctx.bezierCurveTo(10, -len/2, 10, -len/2, 0, -len);
+      // } else {
+      //     ctx.bezierCurveTo(-10, -len/2, -10, -len/2, 0, -len);
+      // }
+      ctx.lineTo(0, -len);
+      ctx.strokeStyle = "yellowgreen";
+      ctx.fillStyle = "green";
+      ctx.lineWidth = branchWidth;
+      // ctx.shadowBlur = 15;
+      // ctx.shadowColor = "rgba(0,0,0,0.8)";
+      ctx.stroke();
+
+      if (len < 10) {
+        ctx.beginPath();
+        ctx.arc(0, -len, 10, 0, Math.PI/2);
+        ctx.fillStyle = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+        ctx.fill();
+        ctx.restore();
+        return;
+      }
+      var p = Math.random(0.2, 0.7) + 0.2;
+
+      this.draw(0, -len, len*p.toFixed(2), angle+10, branchWidth*0.95);
+      this.draw(0, -len, len*p.toFixed(2), angle-10, branchWidth*0.95);
+
+      ctx.restore();
+    }
+  },
+  mounted: function() {
+    this.$refs.treeCanvas.width = window.innerWidth;
+    this.$refs.treeCanvas.height = window.innerHeight;
+    
+    this.draw(350, 600, 120, 0, 5);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
