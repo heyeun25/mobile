5522dadeb137f7a9fed2bceceff230007de49d64
hyeeun park <h514.park@samsung.com>
2020-02-11 14:22:35 +0900

    apply animation
    Former-commit-id: 467fe450181e80aa01c94f3562ba15172382d4df

--- new-shelf/src/components/Tree.vue
+++ new-shelf/src/components/Tree.vue
@@ -6,9 +6,38 @@
 </template>
 
 <script>
-
 // var canvasEl;
-// var ctx;
+var ctx;
+var branches = [];
+var rAF;
+
+function Line(x, y, len, angle, width) {
+  this.originX = x;
+  this.originY = y;
+  this.px = x;
+  this.py = y;
+  this.x = x;
+  this.y = y;
+  this.v = 0.1;
+  this.len = len;
+  this.angle = angle;
+  this.width = width;
+  this.endX = x - parseInt(len * Math.sin(angle * Math.PI/180));
+  this.endY = y - parseInt(len * Math.cos(angle * Math.PI/180));
+  this.branch = false;
+  // console.log('end', this.endX, this.endY, 'start', this.x, this.y);
+  this.done = false;
+  this.update = function() {
+    // update
+    this.v += 0.1;
+    this.px = this.x;
+    this.py = this.y;
+    this.x = this.originX - len * this.v.toFixed(1) * Math.sin(angle * Math.PI/180);
+    this.y = this.originY - len * this.v.toFixed(1) * Math.cos(angle * Math.PI/180);
+    if (this.v > 1) this.done = true;
+    // console.log(this.x, this.y);
+  }
+}
 
 export default {
   name: 'Tree',
@@ -18,46 +47,89 @@ export default {
   methods: {
     draw: function(startX, startY, len, angle, branchWidth) {
       let ctx = this.$refs.treeCanvas.getContext("2d");
-      ctx.beginPath();
-      ctx.save();
-      
-      // ctx.translate(startX, startY);
-      // ctx.rotate(angle * Math.PI/180);
+      // ctx.beginPath();
+      // ctx.save();
 
-      var rotateX = len * Math.sin(angle * Math.PI/180);
-      var rotateY = len * Math.cos(angle * Math.PI/180);
-      // console.log(rotateX, rotateY);
+      // var rotateX = len * Math.sin(angle * Math.PI/180);
+      // var rotateY = len * Math.cos(angle * Math.PI/180);
+      // branches.push({start: {x: startX, y: startY},
+      //                end: {x: startX - rotateX, y: startY - rotateY},
+      //                width: branchWidth});
 
-      ctx.moveTo(startX, startY);
-      // if(angle > 0) {
-      //     ctx.bezierCurveTo(10, -len/2, 10, -len/2, 0, -len);
-      // } else {
-      //     ctx.bezierCurveTo(-10, -len/2, -10, -len/2, 0, -len);
-      // }
-      ctx.lineTo(startX - rotateX, startY - rotateY);
-      ctx.strokeStyle = "darkgreen";
-      ctx.fillStyle = "green";
-      ctx.lineWidth = branchWidth;
-      // ctx.shadowBlur = 15;
-      // ctx.shadowColor = "rgba(0,0,0,0.8)";
-      ctx.stroke();
+      branches.push(new Line(startX, startY, len, angle, branchWidth));
+
+      function d() {
+        // var check = false;
+        for(var i =0; i<branches.length; i++) {
+              ctx.beginPath();
+              ctx.strokeStyle = "darkgreen";
+              ctx.moveTo(branches[i].px, branches[i].py);
+              ctx.lineTo(branches[i].x, branches[i].y);
+              ctx.stroke();
+              branches[i].update();
+              if (branches[i].done == true) {
+                var out = branches.shift();
+                if (out.len > 10) {
+                  var p = Math.random(0.2, 0.7) + 0.2;
+                  p = 0.5;
+                  branches.push(new Line(out.endX, out.endY,
+                      (out.len*p).toFixed(5), out.angle+15, out.branchWidth*0.95));
+                  branches.push(new Line(out.endX, out.endY,
+                      (out.len*p).toFixed(5), out.angle-15, out.branchWidth*0.95));
+                }
+              }
+        }
+
+        // if (check == false) {
+        //     var keep = false;
+        //     for(var i =0; i<branches.length; i++) {
+        //       var target = branches[i];
+        //       if (target.branch == false && target.len > 10) {
+        //           keep = true;
+        //           var p = Math.random(0.2, 0.7) + 0.2;
+        //           // p = 0.8
+        //           branches.push(new Line(target.endX, target.endY,
+        //               (target.len*p).toFixed(2), target.angle+15, target.branchWidth*0.95));
+        //           branches.push(new Line(target.endX, target.endY,
+        //               (target.len*p).toFixed(2), target.angle-15, target.branchWidth*0.95));
+        //           // requestAnimationFrame(d);
+        //           target.branch = true;
+        //       }
+        //     }
+        //     if (keep == false && i == branches.length) {
+        //       cancelAnimationFrame(rAF);
+        //     }
+        // }
+        rAF = requestAnimationFrame(d);
+      }
+
+
+
+
+      rAF = requestAnimationFrame(d);
+      // ctx.moveTo(startX, startY);
+      // ctx.lineTo(startX - rotateX, startY - rotateY);
+      // ctx.strokeStyle = "darkgreen";
+      // ctx.fillStyle = "green";
+      // ctx.lineWidth = branchWidth;
+      // ctx.stroke();
 
       if (len < 10) {
-        ctx.beginPath();
-        ctx.arc(0, -len, 10, 0, Math.PI/2);
-        if (Math.random() > 0.8)
-          ctx.fillStyle = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-        ctx.fill();
-        ctx.restore();
+        // ctx.beginPath();
+        // var m = angle;
+        // ctx.arc(startX - rotateX, startY - rotateY-4, 5, -(90-m)*Math.PI/180, (90+m)*Math.PI/180);
+        // if (Math.random() > 0.8)
+        // ctx.fillStyle = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+        // ctx.fill();
+        // ctx.restore();
+        console.log(branches)
         return;
       }
       var p = Math.random(0.2, 0.7) + 0.2;
-
-      this.draw(startX - rotateX, startY - rotateY, len*p.toFixed(2), angle+15, branchWidth*0.95);
-      this.draw(startX - rotateX, startY - rotateY, len*p.toFixed(2), angle-15, branchWidth*0.95);
-      // this.draw(0, -len, len*p.toFixed(2), angle-15, branchWidth*0.95);
-
-      ctx.restore();
+      p = 0.5;
+      // this.draw(startX - rotateX, startY - rotateY, len*p.toFixed(2), angle+15, branchWidth*0.95);
+      // this.draw(startX - rotateX, startY - rotateY, len*p.toFixed(2), angle-15, branchWidth*0.95);
+      // ctx.restore();
     }
   },
   mounted: function() {
