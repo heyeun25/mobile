0ad9fe3727044848c8cbd6a466e644a58561f664
hyeeun park <h514.park@samsung.com>
2020-02-13 16:02:46 +0900

    add leaf

--- new-shelf/src/components/Tree.vue
+++ new-shelf/src/components/Tree.vue
@@ -16,29 +16,43 @@ var leaf = [[0, 0], [5, -5], [10, 0], [5, 5], [0, 0]];
 var ADD = 1;
 
 function Line(x, y, len, angle, width) {
-  this.originX = x;
-  this.originY = y;
-  this.px = x;
-  this.py = y;
-  this.x = x;
-  this.y = y;
-  this.v = 0.1;
-  this.len = len;
-  this.angle = angle;
-  this.width = width;
-  this.endX = x - parseInt(len * Math.sin(angle * Math.PI/180));
-  this.endY = y - parseInt(len * Math.cos(angle * Math.PI/180));
-  this.branch = false;
-  this.done = false;
-  this.update = function() {
-    // update
-    this.v += 0.1;
-    this.px = this.x;
-    this.py = this.y;
-    this.x = this.originX - len * this.v.toFixed(1) * Math.sin(angle * Math.PI/180);
-    this.y = this.originY - len * this.v.toFixed(1) * Math.cos(angle * Math.PI/180);
-    if (this.v > 1) this.done = true;
-  }
+    this.originX = x;
+    this.originY = y;
+    this.px = x;
+    this.py = y;
+    this.x = x;
+    this.y = y;
+    this.v = 0.1;
+    this.len = len;
+    this.angle = angle;
+    this.width = width;
+    this.endX = x - parseInt(len * Math.sin(angle * Math.PI/180));
+    this.endY = y - parseInt(len * Math.cos(angle * Math.PI/180));
+    this.branch = false;
+    this.done = false;
+    this.update = function() {
+        // update
+        this.v += 0.1;
+        this.px = this.x;
+        this.py = this.y;
+        this.x = this.originX - len * this.v.toFixed(1) * Math.sin(angle * Math.PI/180);
+        this.y = this.originY - len * this.v.toFixed(1) * Math.cos(angle * Math.PI/180);
+        if (this.v > 1) this.done = true;
+        if (Math.random() > 0.99) {
+            this.makeLeaf(this.x, this.y, this.angle);
+        }
+    }
+    this.makeLeaf = function(x, y, angle) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.quadraticCurveTo(
+                x - (20 * Math.cos((45 + angle) * Math.PI/180)),
+                y + (20 * Math.sin((45 + angle) * Math.PI/180)),
+                x - (40 * Math.cos((angle) * Math.PI/180)),
+                y + (40 * Math.sin((angle) * Math.PI/180)));
+        ctx.fillStyle="darkgreen";
+        ctx.fill();
+    }
 }
 
 export default {
@@ -48,7 +62,6 @@ export default {
   },
   methods: {
     draw: function(startX, startY, len, angle, branchWidth) {
-      let ctx = this.$refs.treeCanvas.getContext("2d");
       branches.push(new Line(startX, startY, len, angle, branchWidth));
 
       function d() {
@@ -57,8 +70,6 @@ export default {
               ctx.strokeStyle = "darkgreen";
               ctx.moveTo(branches[i].px, branches[i].py);
               ctx.lineTo(branches[i].x, branches[i].y);
-            //   ctx.strokeStyle = "rgba(224, 103, 67, 1)";
-                ctx.strokeStyle
               ctx.lineWidth = 2;
               ctx.stroke();
               branches[i].update();
@@ -69,28 +80,20 @@ export default {
                 var out = branches.shift();
                 if (out.len > 10) {
                     var p = Math.random(0.2, 0.7) + 0.2;
-                    // p = 0.5;
+                    p = 0.5;
                     branches.push(new Line(out.endX, out.endY,
                         (out.len*p).toFixed(2), out.angle+20, out.branchWidth*0.95));
                     branches.push(new Line(out.endX, out.endY,
                         (out.len*p).toFixed(2), out.angle-20, out.branchWidth*0.95));
                     // ctx.beginPath();
                     // ctx.moveTo(out.endX, out.endY);
-
-
-
                     // ctx.quadraticCurveTo(
-                    //         out.endX + (10 * Math.cos((20 + out.angle) * Math.PI/180)),
-                    //         out.endY - (10 * Math.sin((20 + out.angle) * Math.PI/180)),
-                    //         out.endX + (20 * Math.cos((out.angle) * Math.PI/180)),
-                    //         out.endY - (20 * Math.sin((out.angle) * Math.PI/180)));
-                    // ctx.strokeStyle="red";
-
-
-
-                    ctx.stroke();
-
-
+                    //         out.endX - (20 * Math.cos((45 + out.angle) * Math.PI/180)),
+                    //         out.endY + (20 * Math.sin((45 + out.angle) * Math.PI/180)),
+                    //         out.endX - (40 * Math.cos((out.angle) * Math.PI/180)),
+                    //         out.endY + (40 * Math.sin((out.angle) * Math.PI/180)));
+                    // ctx.fillStyle="darkgreen";
+                    // ctx.fill();
                 } else {
                     // ctx.beginPath();
                     // ctx.moveTo(out.endX, out.endY);
@@ -112,7 +115,11 @@ export default {
   mounted: function() {
     this.$refs.treeCanvas.width = window.innerWidth;
     this.$refs.treeCanvas.height = window.innerHeight;
-    this.draw(window.innerWidth/2, window.innerHeight, 120, 0, 5);
+    ctx = this.$refs.treeCanvas.getContext("2d");
+    this.draw(window.innerWidth/2, window.innerHeight, 200, 0, 5);
+  },
+  unmounted: function() {
+      cancelAnimationFrame(rAF);
   }
 }
 </script>
@@ -122,6 +129,8 @@ img {
     position: fixed;
     left: 0;
     top: 0;
+    width: 100%;
+    height: 100%;
 }
 canvas {
     position: fixed;
