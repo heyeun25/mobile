1150005c794fb4deeb4e7d46afb6daef44bfc776
hyeeun park <h514.park@samsung.com>
2020-04-29 17:32:38 +0900

    add tapestry
    Former-commit-id: 779a18f507d407888b7ad701f8992fe03e1877db

---
+++ new-shelf/src/pages/Tapestry.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="tapestry">
+        <Video
+            width="100%"
+            height="100%"
+            v-bind:source="source"
+        >
+        </Video>
+    </div>
+</template>
+<script>
+import Video from '@/components/Video.vue'
+var getMobile;
+export default {
+    components: {
+        Video,
+    },
+    data() {
+        return {
+            source: "",
+        }
+    },
+    methods: {
+        getMobile: function(data) {
+        }
+    },
+    mounted() {
+        getMobile = this.getMobile.bind(this);
+        this.$socket.on('appMsg', getMobile);
+    }
+}
+</script>
+
+<style>
+
+</style>
+
